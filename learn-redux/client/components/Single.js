8481ae5195e030ce939961fdb84870ad1382cdaa
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-02-11 22:20:21 -0600

    adds data to SinglePage component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 class Single extends Component {
   render() {
+    const { postId } = this.props.params;
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    // get the post
+    const post = this.props.posts[i];
+    // get the comments
+    const postComments = this.props.comments[postId] || [];
     return (
       <div className="single-photo">
-        I'm a single photo
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments}/>
       </div>
-     )
+    );
   }
 }
 
